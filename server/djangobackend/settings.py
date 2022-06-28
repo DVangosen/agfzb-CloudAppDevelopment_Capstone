89cf7a4f46fadbdf3476d1af3845f3d2fbd64325
DVangosen <57511483+DVangosen@users.noreply.github.com>
2022-06-28 15:42:40 -0600

    Update settings.py

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["djvangosen.mybluemix.net", "djvangosen.mybluemix.net/static", "localhost"]
 
 
 # Application definition
